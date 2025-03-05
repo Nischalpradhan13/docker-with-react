9b21b1edb9f8764a4e219baf2064cd3466e45add
PYSCHO@LORD <117845810+Nischalpradhan13@users.noreply.github.com>
2025-03-05 12:21:20 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,6 +26,29 @@ function App() {
         </a>
       </header>
     </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Amazing app Docker
+          with React app
+            Good to go with code
+        </p>
+        <p>
+          Want to edit this <code>src/App.js</code> and go to this and edit. Dami Dami Dami
+          with React app
+            Good to go with code
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
